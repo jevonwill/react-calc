29c38d3c03a5f5d4961ee3cffb2325e83708ac83
jevonwill <jevonwilliamsjr@gmail.com>
2023-01-15 15:32:21 -0500

    Installed dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import Nav from './components/Nav';
+import Members from './pages/Members';
+import Home from './pages/Home';
 function App() {
-  return (
+  {/* Defines routing */}
 
+  let Page
+    switch (window.location.pathname) {
+        case "/":
+            Page = Home
+            break
+        case "/members":
+            Page = Members
+            break
+   }
+    
+  return (
+    <React.StrictMode>
+      <Nav />
+      <Page />
+    </React.StrictMode>
   );
 }
 
---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+import React from 'react';
+//import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+function Nav() {
+    return (
+            <div className='Nav container mx-auto bg-black'>
+                Another Test!              
+           </div>
+    );
+}
+
+export default Nav;
