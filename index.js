8a7de10c804d4cb9b1d7aa4b546c21497979cd1e
jevonwill <jevonwilliamsjr@gmail.com>
2023-01-27 19:58:27 -0500

    Refactored with event listeners

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Math Functions
 const add = function(a, b){
 	let sum = a + b
 	return sum;
@@ -23,15 +24,35 @@ const operate = function(operator, a, b){
 	return result;
 };
 
+//Selectors for buttons
+const zero = document.querySelector('#zero');
+const one = document.querySelector('#one');
+const two = document.querySelector('#two');
+const three = document.querySelector('#three');
+const four = document.querySelector('#four');
+const five = document.querySelector('#five');
+const six = document.querySelector('#six');
+const seven = document.querySelector('#seven');
+const eight = document.querySelector('#eight');
+const nine = document.querySelector('#nine');
+
+
+//Settings input & displays vars to 0
 let lastInput = 0;
 let displayNum = 0;
 
+//Func to set display as the value pressed.
 let setDisplay = function(x){
 	displayNum = x;
 };
 
 
-let operation = function(){
-	lastInput = displayNum;
+//Events Listeners
+one.addEventListener('click', () => {
+	setDisplay(1);
+});
+
+//let operation = function(){
+//	lastInput = displayNum;
 	
 console.log(displayNum);
